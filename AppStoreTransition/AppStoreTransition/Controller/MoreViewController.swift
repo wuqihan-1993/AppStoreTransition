6c5d2b60093001124a3ad3da46b630f9003d2988
wuqihan <wuqihan123@163.com>
2019-09-12 14:41:51 +0800

    Update MoreViewController.swift

--- AppStoreTransition/AppStoreTransition/Controller/MoreViewController.swift
+++ AppStoreTransition/AppStoreTransition/Controller/MoreViewController.swift
@@ -21,34 +21,12 @@ class MoreViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        view.addSubview(testView)
-        
-        testView.snp.remakeConstraints { (make) in
-            make.top.left.equalToSuperview()
-            make.width.equalTo(UIScreen.main.bounds.width)
-            make.height.equalTo(UIScreen.main.bounds.width * 1.34)
-        }
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        testView.snp.remakeConstraints { (make) in
-            make.left.equalToSuperview().offset(38.7)
-            make.top.equalToSuperview().offset(609.926)
-            make.width.equalTo(336.6)
-            make.height.equalTo(464.4)
-        }
-        
-        UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: UIView.AnimationOptions.curveLinear, animations: {
-            
-            self.view.layoutIfNeeded()
-            //((0 0; 414 600.3)
-            
-        }) { (isComplete) in
 
-            
-        }
     }
 
     /*
